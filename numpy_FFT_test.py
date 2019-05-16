746efb85e55abe5bc3b485b530e601ce7ad3e98b
Henk de Vries <b.schuttrups@student.utwente.nl>
2019-05-16 12:04:27 +0200

    Change numpy FFT test for use with timeit()
    Put the back and forth transformation part of the code in a function, so it can be called in  a one-liner using the Python library timeit to evaluate performance.

--- numpy_FFT_test.py
+++ numpy_FFT_test.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy.linalg import norm
 from numpy.fft import *
+from timeit import timeit
 
 def taba(a1):
     '''Transform signal to Fourier domain and back.'''
@@ -9,6 +10,22 @@ def taba(a1):
     A       = fft(a1, **kwargs)          # discrete transform
     a2      = ifft(A, **kwargs)         # discrete siganl after ifft
     return a2
+
+def transf(a, N):
+    '''Take a, make a copy and transform it N times taba.
+    Return the resulting singal and the difference after each
+    transformation pair.'''
+
+    a2      = a.copy()
+    diff    = np.zeros(N)
+    for i in range(N):
+        a2      = taba(a2)
+        diff[i] = norm(a-a2)
+
+    return a2, diff
+
+## Initialization
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 func    = lambda x: np.exp(-x**2)
 
@@ -22,14 +39,19 @@ t       = np.linspace(trange[0], trange[1], Nt)
 a1      = func(t)                       # discrete siganl before fft
 freq    = fftfreq(Nt, d=dt)
 
-a2      = a1
-N       = 100000
-diff    = np.zeros(N)
-print('pass 0, diff.: {}'.format(norm(a1-a2)))
-for i in range(N):
-    a2      = taba(a2)
-    diff[i] = norm(a1-a2)
-    # print('pass {:d}, diff.: {:.3e}'.format(i, diff[i]))
+N       = 10000
+
+## Calculation
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+delta_t = timeit('a2, diff = transf({:d})'.format(N), number=100,
+    setup="from __main__ import transf")
+print('pass {:d}, diff.: {:.3e}'.format(N, diff[N-1]))
+print("Transforming taba {} times took {:.3f} seconds.\nThat's {:.3e} seconds each on average".\
+    format(N, delta_t, delta_t/N))
+
+## Visualization
+##%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 # plot normed difference vs. nr of transforms
 plt.figure(1)
@@ -43,19 +65,11 @@ plt.grid()
 # plot original and N times back-and-forth transformed signal
 plt.figure(2)
 plt.plot(t, a1, 'k--')
-plt.plot(t, a2, 'k')
+plt.plot(t, np.abs(a2), 'k')
 plt.xlim(trange)
 plt.title('signal')
 plt.xlabel('$t$')
 plt.ylabel('a')
 plt.grid()
-
-# plt.figure(3)
-# plt.plot(fftshift(f), fftshift(np.abs(A)), 'k--')
-# plt.xlim([-fs,fs])
-# plt.title('transform')
-# plt.xlabel('$\omega$')
-# plt.ylabel('A')
-# plt.grid()
 
 plt.show()
